2ad297c09d993f20d67e964a9f3c320655b168d6
Felix <vip.zatebe@mail.ru>
2023-05-02 19:44:15 +0300

    Refactor SecondMaxSqmFunction

--- src/main/java/com/test/hibernate6customfunctions/fuctions/SecondMaxSqmFunction.java
+++ src/main/java/com/test/hibernate6customfunctions/fuctions/SecondMaxSqmFunction.java
@@ -1,13 +1,16 @@
-package com.example.hibernate6teststand.fuctions;
+package com.test.hibernate6customfunctions.fuctions;
 
-import com.example.hibernate6teststand.entities.Employee;
 import org.hibernate.query.sqm.function.AbstractSqmSelfRenderingFunctionDescriptor;
+import org.hibernate.query.sqm.function.FunctionKind;
 import org.hibernate.query.sqm.produce.function.ArgumentTypesValidator;
 import org.hibernate.query.sqm.produce.function.StandardArgumentsValidators;
 import org.hibernate.query.sqm.produce.function.StandardFunctionReturnTypeResolvers;
+import org.hibernate.sql.ast.Clause;
 import org.hibernate.sql.ast.SqlAstTranslator;
 import org.hibernate.sql.ast.spi.SqlAppender;
 import org.hibernate.sql.ast.tree.SqlAstNode;
+import org.hibernate.sql.ast.tree.expression.Distinct;
+import org.hibernate.sql.ast.tree.expression.Expression;
 import org.hibernate.sql.ast.tree.predicate.Predicate;
 import org.hibernate.sql.ast.tree.select.SortSpecification;
 import org.hibernate.type.StandardBasicTypes;
@@ -17,36 +20,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class SecondMaxSqmFunction extends AbstractSqmSelfRenderingFunctionDescriptor {
-    private TypeConfiguration typeConfiguration;
 
-    public SecondMaxSqmFunction(
-            String name,
-            TypeConfiguration typeConfiguration
-    ) {
+    public SecondMaxSqmFunction(String name, TypeConfiguration typeConfiguration) {
         super(
                 name,
+                FunctionKind.NORMAL,
                 new ArgumentTypesValidator(StandardArgumentsValidators.exactly(0)),
                 StandardFunctionReturnTypeResolvers.invariant(typeConfiguration.getBasicTypeRegistry().resolve(StandardBasicTypes.BIG_INTEGER)),
                 null
         );
-        this.typeConfiguration = typeConfiguration;
     }
 
     @Override
-    public void render(
-            SqlAppender sqlAppender,
-            List<? extends SqlAstNode> sqlAstArguments,
-            SqlAstTranslator<?> walker) {
-        render(sqlAppender, sqlAstArguments, null, Collections.emptyList(), walker);
-    }
-
-    @Override
-    public void render(
-            SqlAppender sqlAppender,
-            List<? extends SqlAstNode> sqlAstArguments,
-            Predicate filter,
-            SqlAstTranslator<?> walker) {
-        render(sqlAppender, sqlAstArguments, filter, Collections.emptyList(), walker);
+    public void render(SqlAppender sqlAppender, List<? extends SqlAstNode> sqlAstArguments, SqlAstTranslator<?> walker) {
+        render(sqlAppender, sqlAstArguments, null, walker);
     }
 
     @Override
@@ -54,10 +41,8 @@ public class SecondMaxSqmFunction extends AbstractSqmSelfRenderingFunctionDescri
             SqlAppender sqlAppender,
             List<? extends SqlAstNode> sqlAstArguments,
             Predicate filter,
-            List<SortSpecification> withinGroup,
             SqlAstTranslator<?> translator) {
-//        String e = typeConfiguration.getJavaTypeRegistry().resolveEntityTypeDescriptor(Employee.class).getRecommendedJdbcType(typeConfiguration.getCurrentBaseSqlTypeIndicators()).getFriendlyName();
-
-        sqlAppender.appendSql("max(salary) from Employee where salary <> (select max(salary) from Employee)");
+        sqlAppender.appendSql(getName());
+        sqlAppender.appendSql("()");
     }
 }
