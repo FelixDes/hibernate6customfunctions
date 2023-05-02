ac17473650cb7d42ad2018b903e8c4ea4a703b56
Felix <vip.zatebe@mail.ru>
2023-05-02 19:43:22 +0300

    Rename

--- src/test/java/com/test/hibernate6customfunctions/Hibernate6CustomFunctionsApplicationTests.java
+++ src/test/java/com/test/hibernate6customfunctions/Hibernate6CustomFunctionsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.hibernate6teststand;
+package com.test.hibernate6customfunctions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
